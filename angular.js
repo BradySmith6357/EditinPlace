6d997477d29be1459a429b7bc3b2a6cef5b9c061
Student <student@RU-023.local>
2016-01-25 12:27:03 -0700

    Finished edit in place exercise

--- angular.js
+++ angular.js
@@ -1,11 +1,18 @@
 angular.module("mainModule", [])
 angular.module("mainModule").controller("mainTroller", ["$scope", function($scope){
 
-
-
-
-
-
-
+$scope.clickr = function(){
+	console.log("Works!");
+	$scope.hideName = true
+	$scope.showBox = true
+}
+$scope.introClickr = function(){
+	$scope.introHide = true;
+	$scope.showIntro = true
+}
+$scope.aboutClickr = function(){
+	$scope.aboutHide = true;
+	$scope.showAbout = true;
+}
 	
 }])
